b0bef167e2f703db9f58bd279680299dcdca24b0
0xdwong <0xdwong@gmail.com>
2023-03-29 14:28:42 +0800

    添加 timer 模块

---
+++ packages/timer/scripts/heartbeat.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const axios = require('axios');
+const logger = require('../logger');
+
+
+async function main(){
+    const result = await axios({
+        "method": "GET",
+        "url": "https://api.decert.me/health",
+        "headers": {
+            "address": "0x4dA00F03E056ff7d37223c6dB7dc2690dcC99dD4",
+            "Content-Type": "application/json; charset=utf-8"
+        }
+    })
+
+    if(result.status === 200 && result.statusText === 'OK') {
+        logger.info('heartbeat succeed');
+        return true;
+    }
+
+    // 失败的话，在这里添加逻辑
+    logger.error('heartbeat failed', {status: result.status});
+    return false;
+}
+
+main().then((result) =>{
+    process.exit(0);
+}).catch(err => {
+    console.error(err);
+    process.exit(1);
+})
+
