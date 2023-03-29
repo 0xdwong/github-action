88f4b2c71c165af21b43389f9c989a8fbb89073a
0xdwong <0xdwong@gmail.com>
2023-03-29 15:48:46 +0800

    test 触发

--- packages/nodejs_app/src/routes/app.js
+++ packages/nodejs_app/src/routes/app.js
@@ -3,7 +3,7 @@ async function ping(ctx) {
 }
 
 async function hi(ctx) {
-    const msg = 'foo';
+    const msg = 'tom';
     let resp = { 'code': '1', 'msg': msg };
     ctx.response.body = resp;
 }
